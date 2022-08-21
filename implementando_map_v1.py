0b8d605cc66b737945a2289342e4356fc4ee2b71
Wine Ferreira <106812435+Wineferreira@users.noreply.github.com>
2022-08-21 02:18:22 -0300

    initial commit

---
+++ implementando_map_v1.py
@@ -0,0 +1,10 @@
+#!python
+
+def mapear(funcao, lista):
+    for elemento in lista:
+        yield funcao(elemento)
+
+
+if __name__ == "__main__":
+    resultado = mapear(lambda x: x ** 2, [2, 3, 4])
+    print(list(resultado))
---
+++ implementando_map_v2.py
@@ -0,0 +1,8 @@
+#!python
+def mapear(funcao, lista):
+    return (funcao(elemento) for elemento in lista)
+
+
+if __name__ == "__main__":
+    resultado = mapear(lambda x: x ** 2, [2, 3, 4])
+    print(tuple(resultado))
